e4a2c190f58d49db1ff41c24d01861a015eb8eb4
parteek kumar <digitalparteek@gmail.com>
2023-07-01 13:43:05 +0530

    Basic mongodb setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const connect = require("./config/database");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  await connect();
+  console.log("Mongo db connected");
+});
