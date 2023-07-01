3ad0abe45a0303cf0c06645b44249b0431e3aa52
parteek kumar <digitalparteek@gmail.com>
2023-07-01 13:56:11 +0530

    Basic weet db

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connect = require("./config/database");
 
 const app = express();
+const Tweet = require("./models/tweet");
 
 const PORT = 3000;
 
@@ -9,4 +10,9 @@ app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   await connect();
   console.log("Mongo db connected");
+  const tweet = await Tweet.create({
+    content: "Third tweet",
+    // userEmail: "user@example.com",
+  });
+  console.log(tweet);
 });
---
+++ src/models/tweet.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  userEmail: {
+    type: String,
+  },
+});
+
+const Tweet = mongoose.model("Tweet", tweetSchema);
+module.exports = Tweet;
