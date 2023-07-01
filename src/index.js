b76c61cf9c162d70cfcec8a88b8f56f9e40657a7
parteek kumar <digitalparteek@gmail.com>
2023-07-01 14:23:42 +0530

    Tweet crud setup

--- src/index.js
+++ src/index.js
@@ -6,13 +6,26 @@ const Tweet = require("./models/tweet");
 
 const PORT = 3000;
 
+const TweetRepository = require("./repository/tweet-repository");
+
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   await connect();
   console.log("Mongo db connected");
-  const tweet = await Tweet.create({
-    content: "Third tweet",
-    // userEmail: "user@example.com",
-  });
+  //   const tweet = await Tweet.create({
+  //     content: "4th tweet",
+  //     userEmail: "user4@example.com",
+  //   });
+
+  // const tweet = await Tweet.find({
+  //   userEmail: "user@example.com",
+  // });
+
+  // const tweet = await Tweet.findById("649fe3606802b54d9a599907");
+  // tweet.userEmail = "b@c.com";
+  // await tweet.save();
+
+  const tweetRepo = new TweetRepository();
+  const tweet = await tweetRepo.get("649fe3606802b54d9a599907");
   console.log(tweet);
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const tweetSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  userEmail: {
-    type: String,
+const tweetSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    userEmail: {
+      type: String,
+    },
   },
-});
+  { timestamps: true }
+);
 
-const Tweet = mongoose.model("Tweet", tweetSchema);
+const Tweet = mongoose.model("Tweet", tweetSchema); // mongo auto names plural
 module.exports = Tweet;
